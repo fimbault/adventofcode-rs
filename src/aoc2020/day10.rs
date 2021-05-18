cb4d6f3faec19cb0291dd89d1c485e38f1926672
fimbault <45664395+fimbault@users.noreply.github.com>
2021-05-18 18:19:00 +0200

    fix warning

--- src/aoc2020/day10.rs
+++ src/aoc2020/day10.rs
@@ -4,18 +4,19 @@ pub fn challenge() {
     // get a vector filled with puzzle numbers
     let input = get_puzzle(2020, 10).unwrap();
 
-    let mut jolts = input
+    let data = input
         .lines()
         .map(|line| line.parse().unwrap())
         .collect::<Vec<usize>>();
 
-    //let mut counts = [0, 0];
-    let counts = chain_p1(&mut jolts);
+    let counts = chain_p1(&data);
     println!("day 10 (part 1): value = {}", counts[0] * counts[1]);
     println!("day 10 (part 2): not implemented");
 }
 
-fn chain_p1(jolts: &mut Vec<usize>) -> [usize; 2] {
+fn chain_p1(data: &Vec<usize>) -> [usize; 2] {
+    let mut jolts = data.clone();
+
     // re-order
     jolts.sort();
 
@@ -23,8 +24,7 @@ fn chain_p1(jolts: &mut Vec<usize>) -> [usize; 2] {
     jolts.insert(0, 0);
 
     // add a 3 jolt at the end
-    let max = jolts.iter().max().unwrap();
-    jolts.push(*max + 3);
+    jolts.push(jolts.iter().max().unwrap() + 3);
 
     // compute diff between 2 successive values
     // here done using windows but zip would also work
@@ -43,8 +43,8 @@ fn chain_p1(jolts: &mut Vec<usize>) -> [usize; 2] {
 
 #[test]
 fn day10_p1_example1_works() {
-    let mut example: Vec<usize> = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
-    let counts = chain_p1(&mut example);
+    let example: Vec<usize> = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+    let counts = chain_p1(&example);
 
     assert_eq!(counts[0], 7);
     assert_eq!(counts[1], 5);
