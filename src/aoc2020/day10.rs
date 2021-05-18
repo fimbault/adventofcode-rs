79b7abc8d7c7e2f36343b0ae207ee5d6ecf21af1
fimbault <45664395+fimbault@users.noreply.github.com>
2021-05-18 15:38:02 +0200

    day 10 - start

--- src/aoc2020/day10.rs
+++ src/aoc2020/day10.rs
@@ -4,6 +4,6 @@ pub fn challenge() {
     // get a vector filled with puzzle numbers
     let input = get_puzzle(2020, 10).unwrap();
 
-    println!("day 10 (part 1): not implemented);
+    println!("day 10 (part 1): not implemented");
     println!("day 10 (part 2): not implemented");
 }
